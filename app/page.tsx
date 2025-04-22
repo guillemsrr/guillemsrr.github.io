2a3a33a43e58da4b0959d7d457a0b0d85e9ef92c
Guillem Serra <guillemserralorenz@gmail.com>
2025-04-22 12:15:13 +0200

    static config for github pages

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Page()
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
                 About
             </h1>
+
             <p className="mb-4">
                 {`Game programmer with 5+ years of experience using Unreal Engine (C++) and Unity, combining technical depth with a strong sense of design and structure. Projects range from indie solo development to collaborative studio teams. Committed to writing maintainable, efficient code using SOLID principles, and experienced in creating branching narrative systems, gameplay mechanics, and tools for designers.`}
             </p>
