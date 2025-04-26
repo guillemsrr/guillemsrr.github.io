740a7a3a8099cb27315a512339a21c9cc0877be0
Guillem Serra <guillemserralorenz@gmail.com>
2025-04-26 20:57:18 +0200

    Fix basePath and assetPrefix formatting in next.config.ts
    Updated the repoName to include a leading slash and adjusted basePath and assetPrefix handling for consistency. These changes ensure proper URL settings in production environments.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-﻿const repoName = "guillemsrr.github.io"; // <-- CHANGE THIS!
+﻿const repoName = "/guillemsrr.github.io";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : '',
-    assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : '',
+    basePath: process.env.NODE_ENV === "production" ? repoName : '',
+    assetPrefix: process.env.NODE_ENV === "production" ? `${repoName}/` : '',
     images: {
         unoptimized: true,
     },
