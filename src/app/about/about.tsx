9c6bef60069342ce3b8bed6ab4024048b09fad1a
Guillem Serra <guillemserralorenz@gmail.com>
2025-04-27 15:41:52 +0200

    Add new assets, update about section, and improve UI structure
    Added new SVG and PNG assets and updated the About and professional experience sections for better readability and consistency. Enhanced UI by refining layouts, tags, and orientations across components, along with minor refactoring for cleaner code organization.

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -8,8 +8,8 @@ async function getAboutMdx()
 {
     const filePath = path.join(process.cwd(), 'src/app/about/about.mdx');
     const fileContent = fs.readFileSync(filePath, 'utf8');
-    const {content} = matter(fileContent);
-    return content;
+    //const {content} = matter(fileContent);
+    return fileContent;
 }
 
 export default async function About()
@@ -21,21 +21,20 @@ export default async function About()
             <h2 className="font-bold text-3xl mb-8 tracking-tighter">
                 About me
             </h2>
-            <div className="prose prose-neutral dark:prose-invert">
+            <div className="prose prose-neutral">
                 <div className="flex flex-col md:flex-row gap-8">
+
                     <Image
                         alt="Guillem Serra"
-                        src="/images/foto_Guillem.jpg"
-                        width={100}
+                        src="/images/foto_Guillem.png"
+                        width={250}
                         height={100}
-                        priority
-                        className="rounded-full bg-neutral-100 dark:bg-neutral-800"
+                        className="flex-grow object-cover"
                     />
-                    <div className="flex flex-col gap-6">
-                        <p className="mb-4">
-                            {`
-I’m a game programmer with 5+ years of experience using Unreal Engine (C++) and Unity, on projects ranging from indie solo development to collaborative studio teams. Committed to writing scalable, efficient code grounded in established design principles and robust software architectures. I’m always looking to expand my knowledge and skills while creating meaningful and impactful games.                 `}
-                        </p>
+                    <div className="flex flex-col gap-6 flex-shrink">
+                        <p className="mb-4">{`
+I’m a programmer with 5+ years of experience using Unreal Engine (C++) and Unity, on projects ranging from indie solo development to collaborative studio teams. Committed to writing scalable & efficient code focusing on establishing design principles and building robust software architectures. I’m always looking to expand my knowledge and skills while creating meaningful and impactful experiences.                   
+                        `}</p>
                     </div>
                 </div>
                 <CustomMDX source={aboutMdx}/>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,26 @@
+import LinkIcon from "@app/(header)/link-icon";
+
 export default function Footer()
 {
     return (
-        <footer className="mb-16">
-            <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-                © {new Date().getFullYear()} Guillem Serra. All rights reserved.
-            </p>
+        <footer className="mb-8">
+            <div className={'mt-8 flex justify-between'}>
+            <p className="text-neutral-600 dark:text-neutral-300">
+                    © {new Date().getFullYear()} Guillem Serra. All rights reserved.
+                </p>
+                
+                <a
+                    className="flex hover:text-neutral-800 dark:hover:text-neutral-100"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://github.com/guillemsrr/guillemsrr.github.io"
+                >
+
+                    <LinkIcon
+                        path="M16 18l6-6-6-6M8 6l-6 6 6 6"/>
+                    <p className="ml-2">source</p>
+                </a>
+            </div>
         </footer>
     )
 }
