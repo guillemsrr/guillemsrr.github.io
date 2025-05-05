ad02510dddcbc307df3cd2ffe71e102cd7afe8eb
Guillem Serra <guillemserralorenz@gmail.com>
2025-05-05 11:40:26 +0200

    Add configuration for next-export-optimize-images

---
+++ export-images.config.js
@@ -0,0 +1,15 @@
+﻿/**
+ * @type {import('next-export-optimize-images').Config}
+ */
+
+module.exports = {
+  imagesFolderPath: 'public/images',
+  exportFolderPath: 'out',
+  outputFileTracing: false,
+
+  widths: [640, 1280],
+  formats: ['webp'],
+  imageExtensions: [],
+  skipFormat: [],
+  skipFolders: [],
+};
