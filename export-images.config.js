822e9bbe78a8185e7693894135b1d4c5bf065c2f
Guillem Serra <guillemserralorenz@gmail.com>
2025-05-05 13:07:05 +0200

    Refactor image handling and configuration for next-export-optimize-images

--- export-images.config.js
+++ export-images.config.js
@@ -1,15 +1,8 @@
 ﻿/** @type {import('next-export-optimize-images').Config} */
 module.exports = {
-  imagesFolderPath: 'public/images',
-  exportFolderPath: 'out',
-  outputFileTracing: false,
-
-  widths: [640, 1280],
-  formats: ['webp'],
-  imageExtensions: ['jpg', 'jpeg', 'png'],
-  skipFormat: [],
-  skipFolders: [],
-
-  // This removes the original .png files
-  keepOriginalImages: false,
+  convertFormat: [
+    ['png', 'webp'],
+    ['jpg', 'webp'],
+    ['jpeg', 'webp'],
+  ],
 };
