31fc2032c4a91c18b56db645a2d1d577811b2231
Guillem Serra <guillemserralorenz@gmail.com>
2025-05-17 11:47:28 +0200

    added "made by me" on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,21 +5,26 @@ export default function Footer()
     return (
         <footer className="mb-8">
             <div className={'mt-8 flex justify-between'}>
-            <p className="text-neutral-600 dark:text-neutral-300">
+                <p className="text-neutral-600 dark:text-neutral-300">
                     © {new Date().getFullYear()} Guillem Serra. All rights reserved.
                 </p>
-                
-                <a
-                    className="flex px-1 rounded-lg general-anchor"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    href="https://github.com/guillemsrr/guillemsrr.github.io"
-                >
 
-                    <LinkIcon
-                        path="M16 18l6-6-6-6M8 6l-6 6 6 6"/>
-                    <p className="font-sm ml-2">source</p>
-                </a>
+                <div className={"flex items-center general-anchor font-sm"}>
+                    <p className="text-neutral-600 dark:text-neutral-300">made by me</p>
+
+                    <a
+                        className="flex items-center gap-1 hover:underline ml-4"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://github.com/guillemsrr/guillemsrr.github.io"
+                    >
+                        <LinkIcon
+                            path="M16 18l6-6-6-6M8 6l-6 6 6 6"
+                        />
+                        <span>source</span>
+                    </a>
+                </div>
+
             </div>
         </footer>
     )
