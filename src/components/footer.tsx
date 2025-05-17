569eaf7c38dbe167a413185f63a0de5bbbd02061
Guillem Serra <guillemserralorenz@gmail.com>
2025-05-17 13:56:56 +0200

    improved footer for mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,14 @@ export default function Footer()
 {
     return (
         <footer className="mb-8">
-            <div className={'mt-8 flex justify-between'}>
-                <p className="text-neutral-600 dark:text-neutral-300">
-                    © {new Date().getFullYear()} Guillem Serra. All rights reserved.
+            <div className={'mt-8 flex flex-col sm:flex-row justify-between gap-4 sm:gap-0'}>
+                <p className="text-neutral-600 dark:text-neutral-300 text-center sm:text-left">
+                © {new Date().getFullYear()} Guillem Serra. All rights reserved.
                 </p>
 
-                <div className={"flex items-center general-anchor font-sm"}>
-                    <p className="text-neutral-600 dark:text-neutral-300">made by me</p>
+                
+                <div className={"flex items-center justify-center sm:justify-start general-anchor font-sm"}>
+                <p className="text-neutral-600 dark:text-neutral-300">made by me</p>
 
                     <a
                         className="flex items-center gap-1 hover:underline ml-4"
