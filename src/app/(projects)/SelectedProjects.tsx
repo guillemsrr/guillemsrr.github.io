9ecc20bde3d276f7727bb506a6c7d2ed79eb145a
Guillem Serra <guillemserralorenz@gmail.com>
2025-06-16 17:37:06 +0200

    godot wfc update

--- src/app/(projects)/SelectedProjects.tsx
+++ src/app/(projects)/SelectedProjects.tsx
@@ -11,7 +11,7 @@ export async function getSelectedProjects(projectNames: string[]): Promise<Proje
         .filter((p): p is Project => p !== null);
 }
 
-const selectedSlugs = ['vgscript', 'soliloquy', 'archer', 'a-void', 'cellular-automata', 'wfc'];
+const selectedSlugs = ['vgscript', 'soliloquy', 'archer', 'a-void', 'wfc-godot', 'cellular-automata'];
 
 export default async function SelectedProjects()
 {
