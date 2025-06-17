46e1500d738bb885842c4540b2618f442d4b21f9
Guillem Serra <guillemserralorenz@gmail.com>
2025-06-17 11:44:29 +0200

    new resume pdf

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -59,7 +59,7 @@ export default async function About()
                             </p>
                             <div>
                                 <a
-                                    href="/Programmer_CV_GuillemSerra.pdf"
+                                    href="/CV_GuillemSerra.pdf"
                                     download
                                     className="email-button px-4 py-2 rounded-lg bg-gray-600 text-white dark:text-white hover:bg-gray-400 dark:hover:bg-gray-500 transition-colors"
                                 >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function MainPage()
                             Email me
                         </Link>
                         <a
-                            href="/Programmer_CV_GuillemSerra.pdf"
+                            href="/CV_GuillemSerra.pdf"
                             download
                             className="email-button inline-block px-4 py-2 rounded-lg bg-gray-600 text-white hover:bg-gray-400 dark:hover:bg-gray-500 transition-colors"
                         >
