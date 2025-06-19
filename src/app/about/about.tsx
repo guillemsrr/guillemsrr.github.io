8b426bb9d9dd93fea9a42cd1b64f8cffdb69947b
Guillem Serra <guillemserralorenz@gmail.com>
2025-06-19 11:31:22 +0200

    changed .pdf name

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -60,7 +60,7 @@ export default async function About()
                             </p>
                             <div>
                                 <a
-                                    href={imageBaseUrl + "/CV_GuillemSerra.pdf"}
+                                    href={imageBaseUrl + "/CV_GuillemSerra_GameProgrammer.pdf"}
                                     download
                                     className="email-button px-4 py-2 rounded-lg bg-gray-600 text-white dark:text-white hover:bg-gray-400 dark:hover:bg-gray-500 transition-colors"
                                 >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function MainPage()
                             Email me
                         </Link>
                         <a
-                            href={imageBaseUrl + "/CV_GuillemSerra.pdf"}
+                            href={imageBaseUrl + "/CV_GuillemSerra_GameProgrammer.pdf"}
                             download
                             className="email-button inline-block px-4 py-2 rounded-lg bg-gray-600 text-white hover:bg-gray-400 dark:hover:bg-gray-500 transition-colors"
                         >
